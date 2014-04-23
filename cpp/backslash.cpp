aad078af5097b52d87411ea957d2723fd4631246
Jolly Wing <jiqingwu@gmail.com>
2014-04-23 10:42:31 +0800

    start to learn accelerated c++

---
+++ cpp/backslash.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+/* A simple c++ program to examine `\\' */
+int main()
+{
+    string name;
+    cout << "Input your name: ";
+    cin >> name;
+    // OUTPUT: \n represents new line.
+    cout << "Hello, " << name << endl;
+    return 0;
+}
