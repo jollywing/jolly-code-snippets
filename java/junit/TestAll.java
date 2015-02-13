cd0e52aaaed3ea254571a4255e4d93813be3f136
Jolly Wing <jiqingwu@gmail.com>
2015-02-13 16:59:21 +0800

    junit basic

--- java/junit/TestAll.java
+++ java/junit/TestAll.java
@@ -1,3 +1,17 @@
 
+import junit.framework.TestSuite;
+import junit.framework.Test;
+import junit.textui.TestRunner;
+
 public class TestAll extends TestSuite {
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Project Test");
+        suite.addTestSuite(TestCaculator.class);
+        return suite;
+    }
+
+    public static void main(String args[])
+    {
+        TestRunner.run(suite());
+    }
 }
